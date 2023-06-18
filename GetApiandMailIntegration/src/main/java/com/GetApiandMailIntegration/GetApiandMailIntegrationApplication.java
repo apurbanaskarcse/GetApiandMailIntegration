5cd8e7cf90b9d7538f7bcea20c3d41f2209bf981
apurbanaskarcse <apurbanaskarcse@outlook.com>
2023-06-18 21:44:54 +0530

    GetApiandMailIntegration commit

---
+++ GetApiandMailIntegration/src/main/java/com/GetApiandMailIntegration/GetApiandMailIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.GetApiandMailIntegration;
+
+import org.MailHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApiandMailIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GetApiandMailIntegrationApplication.class, args);
+	}
+
+}
